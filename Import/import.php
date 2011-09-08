d40436ae63827010fe04186e7a230a9eb6c8b4c3
Anton Shishkin <a.shishkin.b@gmail.com>
2011-09-08 18:45:26 +0400

    finished artist import script. should test with correct encoding

--- Import/import.php
+++ Import/import.php
@@ -16,15 +16,16 @@ for($i=2;$i<=$max;$i++){
 	$phone = $aSheet->getCell("C".$i)->getValue(); //phone
 	echo " $phone";
 
-	/*$res = mysql_query("SELECT * from 'artists' WHERE fio='$fio'");
-	while($row = mysql_fetch_array($res)){
-		echo "already exists";
+	$res = mysql_query("SELECT * from artists WHERE fio='$fio'");
+	if($res != NULL){
+		$row = mysql_fetch_array($res);
+		echo " already exists<br>";
 		continue;
-	}*/
-	$query = "INSERT INTO 'artists' (fio,phone) VALUES '".$fio."','".$phone."'";
-	//echo "<br>".$query."<br>";
+	}
+
+	$query = "INSERT INTO artists (fio,phone) VALUES ('$fio','$phone')";
 	mysql_query($query) or die(" fail");
-	echo "ok";
+	echo " ok<br>";
 }
-//echo "ok";
+echo "<br>$i ok";
 ?>
