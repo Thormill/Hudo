cf4a34a5aef7ae04187068e694fe148b5485df4e
Stepan Kirushkin <stepan@leadsleader.ru>
2011-09-08 17:29:49 +0400

    Added something new

---
+++ forms/fAdd.php
@@ -0,0 +1,9 @@
+<form>
+    <label for="user_fio">ФИО: </label><input id="user_fio" type="text"></input>
+    <label for="view">Вид: </label><select id="view"></select>
+    <label for="category">Категория: </label><select id="category"></select>
+    <label for="product">Изделие: </label><select id="product"></select>
+    <label for="count">Количество: </label><select id="count"><? for($i = 0; $i < 15; $i++) print('<option value ='.$i.'>'.$i.'</option>') ?></select>
+    <label for="cost">Цена: </label><input id="cost" type="text"></input>
+    <input type="submit" value="Добавить"></input>
+</form>
