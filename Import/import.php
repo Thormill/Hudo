2a3db63cb4db1eefed8da5e4d2ac48ab416b3be8
Anton Shishkin <a.shishkin.b@gmail.com>
2011-09-08 18:22:05 +0400

    minor improvements

--- Import/import.php
+++ Import/import.php
@@ -1,3 +1,4 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <?php
 require_once '../lib/db.php';
 require_once '../lib/phpExcel/PHPExcel.php';
@@ -7,14 +8,23 @@ include("../lib/phpExcel/PHPExcel/Writer/Excel5.php");
 $objPHPExcel = PHPExcel_IOFactory::load("given.xls");
 $objPHPExcel->setActiveSheetIndex(0); //художники
 $aSheet = $objPHPExcel->getActiveSheet();
-
+//mysql_query("INSERT INTO artists VALUES 'test','test'") or die("lolo");
 $max =  $aSheet->getHighestRow();
 for($i=2;$i<=$max;$i++){
-$fio = $aSheet->getCell("B".$i)->getValue()."\r\n"."<br>"; //fio
-$phone = $aSheet->getCell("C".$i)->getValue()."\r\n"."<br>"; //phone
-$query = "INSERT INTO artists (fio, phone) VALUES ".$fio.", ".$phone;
-echo $query;
-mysql_query($query) or die("fail while inserting");
+	$fio = $aSheet->getCell("B".$i)->getValue(); //fio
+	echo "$fio";
+	$phone = $aSheet->getCell("C".$i)->getValue(); //phone
+	echo "$phone";
+
+	$row = mysql_query("SELECT * from 'artists' WHERE fio='$fio'");
+	if(mysql_fetch_array($row)){
+		echo "already exists";
+		continue;
+	}
+	$query = "INSERT INTO 'artists' (fio,phone) VALUES '$fio','$phone'";
+	//echo "<br>".$query."<br>";
+	mysql_query($query) or die(" fail");
+	echo "ok";
 }
-echo "ok";
+//echo "ok";
 ?>
