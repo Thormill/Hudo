68d10ff5f517b97c7204be6232e3680adcad4c83
Anton Shishkin <a.shishkin.b@gmail.com>
2011-09-09 18:31:26 +0400

    init

---
+++ forms/ajax/getCategories.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../../config/constants.php';
+require_once '../../classes/database.class.php';
+$oDB = new Database($aDatabase['host'], $aDatabase['user'], $aDatabase['pwd'], $aDatabase['name']);
+
+$aCategories = $oDB->selectColumn('
+    SELECT `category_name`
+        FROM `categories`
+        WHERE `type_id` = ' . $_POST['iType']
+);
+
+echo '<option value="0">--Выберите категорию изделия--</option>';
+foreach ($aCategories as $iCategory => $sCategory)
+    echo '<option value="' . ($iCategory+1) . '">' . $sCategory . '</option>';
---
+++ forms/ajax/getItems.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../../config/constants.php';
+require_once '../../classes/database.class.php';
+$oDB = new Database($aDatabase['host'], $aDatabase['user'], $aDatabase['pwd'], $aDatabase['name']);
+
+$aItems = $oDB->selectColumn('
+    SELECT `item_name`
+        FROM `items`
+        WHERE `category_id` = ' . $_POST['iCategory']
+);
+
+echo '<option value="0">--Выберите изделия--</option>';
+foreach ($aItems as $iItem => $sItem)
+    echo '<option value="' . ($iItem+1) . '">' . $sItem . '</option>';
