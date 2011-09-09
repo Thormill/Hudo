3872d5283a2a4cc2a67f91b38430471376542ff9
Anton Shishkin <a.shishkin.b@gmail.com>
2011-09-09 14:16:42 +0400

    init

--- import/import.php
+++ import/import.php
@@ -4,11 +4,16 @@ require_once '../lib/phpexcel/PHPExcel.php';
 include_once '../lib/phpexcel/PHPExcel/IOFactory.php';
 include("../lib/phpexcel/PHPExcel/Writer/Excel5.php");
 
+iconv_set_encoding("internal_encoding", "UTF-8");
+iconv_set_encoding("input_encoding", "UTF-8");
+iconv_set_encoding("output_encoding", "UTF-8");
+
 $objPHPExcel = PHPExcel_IOFactory::load("given.xls");
 $objPHPExcel->setActiveSheetIndex(0); //художники
 $aSheet = $objPHPExcel->getActiveSheet();
 $max =  $aSheet->getHighestRow();
 $count = $max-1;
+
 for($i=2;$i<=$max;$i++){
 	$fio = $aSheet->getCell("B".$i)->getValue(); //fio
 	echo "$fio";
@@ -20,14 +25,12 @@ for($i=2;$i<=$max;$i++){
 		continue;
 	}
 
-
 	$res = mysql_query("SELECT * from artists WHERE fio='$fio'");
 	if($row = mysql_fetch_array($res) != NULL){
 		$count--;
 		echo " already exists<br>";
 		continue;
 	}
-
 	$query = "INSERT INTO artists (fio,phone) VALUES ('$fio','$phone')";
 	mysql_query($query) or die(" fail");
 	echo " ok<br>";
