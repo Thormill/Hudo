a5239813ec5dfbb1497ef865eebaf547ecb856a2
Anton Shishkin <a.shishkin.b@gmail.com>
2011-09-21 18:50:26 +0400

    запилил даунлоад/download made

--- modules/download.php
+++ modules/download.php
@@ -1,22 +1,13 @@
 <?php
-$path = $_SERVER['DOCUMENT_ROOT']."/files/export.xlsx"; // change the path to fit your websites document structure
-$fullPath = $path.$_GET['download_file'];
+$path = "../files/export.xlsx";
 
-if ($fd = fopen ($fullPath, "r")) {
-    $fsize = filesize($fullPath);
-    $path_parts = pathinfo($fullPath);
-    $ext = strtolower($path_parts["extension"]);
-    switch ($ext) {
-        case "xlsx":
-        header("Content-type: application/xlsx"); // add here more headers for diff. extensions
-        header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a download
-        break;
-        default;
-        header("Content-type: application/octet-stream");
-        header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
-    }
+if ($fd = fopen ($path, "r")) {
+    $fsize = filesize($path);
+    $path_parts = pathinfo($path);
+    header("Content-type: application/xlsx");
+    header("Content-Disposition: attachment; filename='export.xlsx'");
     header("Content-length: $fsize");
-    header("Cache-control: private"); //use this to open files directly
+    header("Cache-control: private");
     while(!feof($fd)) {
         $buffer = fread($fd, 2048);
         echo $buffer;
@@ -24,7 +15,6 @@ if ($fd = fopen ($fullPath, "r")) {
 }
 fclose ($fd);
 exit;
-// example: place this kind of link into the document where the file download is offered:
-// <a href="download.php?download_file=some_file.pdf">Download here</a>
 ?>
+
 
