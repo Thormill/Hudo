c70a03e817be7eb7647416942cf7b49ac16d5093
unknown <Wisp@.(none)>
2011-09-26 00:23:56 +0400

    changed control form. removed add master page - its functional now is on data control form

--- forms/fControl.php
+++ forms/fControl.php
@@ -10,27 +10,57 @@ require_once ROOT . 'database.class.php';
 $oDB = new Database($aDatabase['host'], $aDatabase['user'], $aDatabase['pwd'], $aDatabase['name']);
 ?>
 
-<form id="fControl" class="fControl">
+<form id="ItemControl" class="fControl">
   <div>
-    <p>Здесь можно добавить тип, категорию и само изделие, а также указать его стоимость.<br>
-      <label for="atype">Тип</label><input type="text" id="atype"></input>
-      <label for="acategory">Категория</label><input type="text" id="acategory"></input>
-      <label for="aitem">Изделие</label><input type="text" id="aitem"></input>
-      <label for="aprice">Цена</label><input type="text" id="aprice"></input>
-      <input type="button" value="Добавить" onClick="ItemAdd();"/>
-    </p>
-  </div>
-  <div class="cMasters">
-    <p>Здесь можно отредактировать мастеров</p>
-      <p><label>Фио:</label>    <input type="text"></input><SELECT name="fio" /></p>
-      <p><label>Телефон:</label><input type="text"></input><SELECT name="phone" /></p>
-  </div>
-  <div class="dItems">
-    <p>Здесь можно отредактировать существующую категорию.</p>
-      <p><label>Тип</label>      <input type="text"></input><SELECT name="type" /></p>
-      <p><label>Категория</label><input type="text"></input><SELECT name="category" /></p>
-      <p><label>Изделие</label>  <input type="text"></input><SELECT name="item" /></p>
-      <p><label>Цена</label>     <input type="text"></input></p>
+    <p>Здесь можно добавить тип, категорию и само изделие, а также указать его стоимость.</p>
+	  <div id="loaded">
+	    <SELECT size="5" class="multiselect" id="mType" onChange="document.getElementById('atype').value = this.options[this.selectedIndex].value">
+	      <?php for($i = 0; $i < 10; $i++) echo "<option value = $i>$i</option>"; ?>
+	    </SELECT>
+		<SELECT size="5" class="multiselect" id="mCategory" onChange="document.getElementById('acategory').value = this.options[this.selectedIndex].value">
+	      <?php for($i = 0; $i < 10; $i++) echo "<option value = $i>$i</option>"; ?>
+	    </SELECT>
+		<SELECT size="5" class="multiselect" id="mItem" onChange="document.getElementById('aitem').value = this.options[this.selectedIndex].value">
+	      <?php for($i = 0; $i < 10; $i++) echo "<option value = $i>$i</option>"; ?>
+	    </SELECT>
+	  </div>
+	  
+	  <div id="edited">
+	    <label for="atype">Тип</label><input type="text" id="atype"></input>
+	    <label for="acategory">Категория</label><input type="text" id="acategory"></input>
+	    <label for="aitem">Изделие</label><input type="text" id="aitem"></input>
+	    <label for="aprice">Цена</label><input type="text" id="aprice"></input>
+        <input type="button" value="Добавить" onClick="ItemAdd();"/>
+	</div>
   </div>
 </form>
 
+<form id="MasterControl" class="fControl">
+  <div class="cMasters">
+    <p>Здесь можно отредактировать мастеров</p>
+    <p>
+	  <label>Фио:</label>
+	  <input type="text" id="fio"></input>
+	  <SELECT onChange="document.getElementById('fio').value = this.options[this.selectedIndex].value">
+	    <?php for($i = 0; $i < 10; $i++) echo "<option value = $i>$i</option>"; ?>
+	  </SELECT>
+	</p>
+    <p><label>Телефон:</label><input type="text"></input></p>
+	<input type="button" onClick="" value="изменить" />
+  </div>
+</form>
+
+<form id="PremadeControl" class="fControl">
+  <div>
+    <p>Здесь можно отредактировать заготовки</p>
+	<div class="dItems">
+	  <SELECT size="5" class="multiselect" id="mItem" onChange="document.getElementById('premade').value = this.options[this.selectedIndex].value">
+	    <?php for($i = 0; $i < 10; $i++) echo "<option value = $i>$i</option>"; ?>
+	  </SELECT>      
+    </div>
+	<div>
+	  <label for="premade">Название</label><input type="text" id="premade"></input>	  
+	</div>
+    <input type="button" onClick="" value="изменить" />
+  </div>
+</form>
