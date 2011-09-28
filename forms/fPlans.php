5e156ea0bf46a1473d4f6b9c8d8c44e534e842d8
Anton Shishkin <a.shishkin.b@gmail.com>
2011-09-28 12:13:37 +0400

    plan base functions released

--- forms/fPlans.php
+++ forms/fPlans.php
@@ -14,20 +14,29 @@ $oDB = new Database($aDatabase['host'], $aDatabase['user'], $aDatabase['pwd'], $
 <form id="PlanControl" class="fControl">
     <p>Здесь можно составить план - указать изделие и требуемое количесто.</p>
   <div id="addplan" class="planadd">
-	<SELECT id="type" name="type">
-		<option>--тип изделия--</option>
+	<SELECT id="typelist" name="type" onChange="TypeClick();">
+	    <?php
+		    $aType = $oDB->selectTable('
+            SELECT `t_id`, `type_name`
+                    FROM `types`
+                    ORDER BY `type_name` ASC'
+          );
+          echo '<option value="0">--Выберите тип изделия--</option>';
+          foreach ($aType as $iType => $aType)
+              echo '<option value="' . $aType['t_id'] . '">' . $aType['type_name'] . '</option>';
+        ?>
 	</SELECT>
 	
-	<SELECT id="category" name="category">
+	<SELECT id="categorylist" name="category" onChange="CategoryClick();">
 		<option>--категория изделия--</option>
 	</SELECT>
 	
-	<SELECT id="item" name="item">
+	<SELECT id="itemlist" name="item" onChange="ItemClick();">
 		<option>--выберите изделие--</option>
 	</SELECT>
 	
 	<input type="text" name="amount"></input>
-	<input type="text" name="price"></input>
+	<input type="text" name="price" id="price"></input>
 	<input type="button" onClick="AddToList();" value="+" />
     
     <input type="button" onClick="Add();" value="добавить" />
--- scripts/fPlans.js
+++ scripts/fPlans.js
@@ -1,5 +1,35 @@
+function TypeClick() {
+	$('#itemlist option').attr('selected', false);
+    $('#itemlist').html('<option>--Сначала выберите категорию--</option>');
+    $('#price').val('');
+    $('#categorylist option').attr('selected', false);
+    $.post('ajax/loadCategories.php', { t_id : $('#typelist option:selected').val()},
+    function (data) {
+       $('#categorylist').html(data);
+    });
+}
+
+function CategoryClick() {
+    $('#itemlist option').attr('selected', false);
+    $('#price').val('');
+    $.post('ajax/loadItems.php', { c_id : $('#categorylist option:selected').val()},
+    function (data) {
+       $('#itemlist').html(data);
+    });
+}
+
+function ItemClick() {
+    $.post('ajax/loadPrice.php', { item_id : $('#itemlist option:selected').val()},
+    function (data) {
+       $('#price').val(data);
+    });
+}
+
 function AddToList() {
 }
 
 function Add() {
+}
+
+function PlanClear() {
 }
