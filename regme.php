d32bebf5400c6d69cf0a6e75946fff1639ca5e52
Anton Shishkin <a.shishkin.b@gmail.com>
2011-10-10 14:01:41 +0400

    multiple login bug resolved. release v.1.0

--- regme.php
+++ regme.php
@@ -5,8 +5,8 @@ require_once ROOT . 'database.class.php';
 session_start();
 $oDB = new Database($aDatabase['host'], $aDatabase['user'], $aDatabase['pwd'], $aDatabase['name']);
 
-if( (!isset($_POST['userlogin'])) || (!isset($_POST['userpass'])) 
-                                  || (!isset($_POST['userinfo'])) )
+if( (!isset($_POST['userlogin'])) && (!isset($_POST['userpass'])) 
+                                  && (!isset($_POST['userinfo'])) )
 {
 ?>
 <html>
@@ -33,6 +33,30 @@ if( (!isset($_POST['userlogin'])) || (!isset($_POST['userpass']))
 <?php
 }
 else {
+	?>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Система учета счетов</title>
+<link rel="shortcut icon" href="img/favicon.ico">
+<link rel="stylesheet" type="text/css" href="style/Index.css" />
+<script type="text/javascript" src="scripts/jquery-min.js"></script>
+<script type="text/javascript" src="scripts/jquery-ui-1.8.16.js"></script>
+<script type="text/javascript" src="scripts/Index.js"></script>
+</head>
+	<?
+	$sLogin = $oDB->selectField('
+	    SELECT `userinfo`
+	    FROM   `users`
+	    WHERE  `uname` = "' . $_POST['userlogin'] . '"
+	');
+	if(empty($sLogin))
+	{
+		unset($_POST['userlogin']);
+        unset($_POST['userpass']);
+        unset($_POST['userinfo']);
+		exit( $sLogin . ' уже зарегистрирован в системе.');
+	}
+	
 	$iInsert = $oDB->insert('
 	    INSERT INTO `users`
 	    SET `uname` = "' . $_POST['userlogin'] . '",
@@ -41,10 +65,16 @@ else {
 	');
     if ($iInsert != 0){
         echo "Пользователь добавлен.";
+        echo '|' . $sLogin . '|';
         unset($_POST['userlogin']);
         unset($_POST['userpass']);
         unset($_POST['userinfo']);
 	}
     else
+    {
         echo "Ошибка БД";
+        unset($_POST['userlogin']);
+        unset($_POST['userpass']);
+        unset($_POST['userinfo']);
+	}
 }
