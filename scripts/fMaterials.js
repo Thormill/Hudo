24419c64669eacc3dde07858e5b52244327a2f6b
Anton Shishkin <a.shishkin.b@gmail.com>
2011-10-21 15:59:56 +0400

    material form ajaxed

--- scripts/fMaterials.js
+++ scripts/fMaterials.js
@@ -2,17 +2,8 @@ function MaterialClick(material) {
 	$.post('ajax/MaterialCheck.php', { id : $(material).attr('id'), amount : $('#amount' + $(material).attr('id')).val() },
     function (data) {
        showMessage(data, 'info');
-    });
-    
-    $.post('ajax/currentMaterialAmount.php', { id : $(material).val() },
-    function (val) {
-        $('#amount' + $(material).val()).val(val);
-	    if (val == 0)
-	        $(material).parent().html('');
-		if($(material).parent().parent().html() == '')
-		{
-		    $(material).parent().parent().parent().html('');
-		}
+    }).done(function(){
+          ShowMaterials();
     });
 }
 
